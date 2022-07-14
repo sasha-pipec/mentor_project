f04e8e616c675b851dfd568f5ad6cd77aa95d40c
Razarenov Aleksandar <razarenov.aleksandar@yandex.ru>
2022-07-14 16:55:53 +0300

    update personal account temlate and update url names

--- photobatle/urls.py
+++ photobatle/urls.py
@@ -4,14 +4,14 @@ from . import views
 urlpatterns = [
     # Классы предствавления
     path('', views.RenderingHomePage.as_view(), name='home'),
-    path('User/', views.RenderingUserPage.as_view(), name='user_page'),
-    path('Photo/get/<slug:slug_id>', views.DetailPost.as_view(), name='detail_post'),
-    path('Comment/patch/<parent_comment_id>', views.CreatingCommentForPhoto.as_view(), name='create_comment'),
-    path('Comment/delete/<comment_pk>', views.DeletingCommentForPhoto.as_view(), name='delete_comment'),
-    path('Comment/put/<comment_pk>', views.UpdatingCommentForPhoto.as_view(), name='update_comment'),
+    path('user/', views.RenderingUserPage.as_view(), name='user_page'),
+    path('photo/get/<slug:slug_id>', views.DetailPost.as_view(), name='detail_post'),
+    path('comment/patch/<parent_comment_id>', views.CreatingCommentForPhoto.as_view(), name='create_comment'),
+    path('comment/delete/<comment_pk>', views.DeletingCommentForPhoto.as_view(), name='delete_comment'),
+    path('comment/put/<comment_pk>', views.UpdatingCommentForPhoto.as_view(), name='update_comment'),
 
-    path('Like/patch/<photo_id>', views.CreatingLikeForPhoto.as_view(), name='create_like'),
-    path('Like/delete/<photo_id>', views.DeletingLikeForPhoto.as_view(), name='delete_like'),
+    path('like/patch/<photo_id>', views.CreatingLikeForPhoto.as_view(), name='create_like'),
+    path('like/delete/<photo_id>', views.DeletingLikeForPhoto.as_view(), name='delete_like'),
 
     # Функции предствавления
     path('logout_user/', views.Logouting_user, name='logout_user'),
