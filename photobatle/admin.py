44393b2c69730fd5d3d9400a6661133b5c35cd96
Razarenov Aleksandar <razarenov.aleksandar@yandex.ru>
2022-08-03 13:30:47 +0300

    update all ModelAdmin

--- photobatle/admin.py
+++ photobatle/admin.py
@@ -1,5 +1,4 @@
-import datetime
-
+from datetime import date
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
@@ -7,7 +6,6 @@ from . import models
 
 
 class UserAdmin(admin.ModelAdmin):
-    '''Переопределенная модель user в админке'''
     list_display = ('username', 'first_name', 'last_name', 'get_html_photo', 'email')
     list_filter = ('first_name', 'last_name', 'is_superuser')
 
@@ -19,13 +17,13 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class PhotoAdmin(admin.ModelAdmin):
-    '''Модель фото в админке'''
     list_display = (
         'user', 'get_html_photo', 'photo_name', 'create_at', 'updated_at',
         'moderation')
-    list_filter = ('moderation',)
-    readonly_fields = ('create_at', 'updated_at',)
+    list_filter = ('moderation', 'user')
     list_editable = ('moderation',)
+    readonly_fields = ('create_at', 'updated_at',)
+    search_fields = ('photo_name',)
     prepopulated_fields = {'slug': ('photo_name',)}
 
     def get_html_photo(self, object):
@@ -36,11 +34,28 @@ class PhotoAdmin(admin.ModelAdmin):
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('content',)
+    list_display = ('photo', 'get_html_photo', 'user', 'content', 'create_at', 'updated_at',)
+    list_filter = ('photo',)
+    search_fields = ('user__username', 'content')
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'фото'
 
 
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ('photo', 'user')
+    list_display = ('get_html_photo', 'user')
+    list_editable = ('user',)
+    list_filter = ('user',)
+    search_fields = ('user__username',)
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'фото'
 
 
 admin.site.register(models.Usermodels.User, UserAdmin)
