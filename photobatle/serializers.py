374d7ddf92b11665178a53856362aff1c192cf18
Razarenov Aleksandar <razarenov.aleksandar@yandex.ru>
2022-08-12 00:42:49 +0300

    add all_comments field from photo serializer

--- photobatle/serializers.py
+++ photobatle/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from photobatle.models import Usermodels, Photomodels, Commentmodels
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    '''Сериализатор для модели User'''
+
+    class Meta:
+        model = Commentmodels.Comment
+        fields = ('__all__')
+
+
 class UserSerializer(serializers.ModelSerializer):
     '''Сериализатор для модели User'''
 
@@ -18,12 +26,16 @@ class PhotoSerializer(serializers.ModelSerializer):
     checking_the_existence = serializers.CharField()
     get_absolute_url = serializers.CharField()
     get_moderation_display = serializers.CharField()
+    all_comments = serializers.SerializerMethodField()
     photo_imagekit_medium = serializers.SerializerMethodField()
 
     def get_photo_imagekit_medium(self, obj):
         return obj.photo_imagekit_medium.url
 
+    def get_all_comments(self, obj):
+        return (CommentSerializer(Commentmodels.Comment.objects.select_related('photo').filter(
+            photo_id=obj.id), many=True)).data
+
     class Meta:
         model = Photomodels.Photo
         fields = ('__all__')
-
