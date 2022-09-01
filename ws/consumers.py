fb103448d90435326b0a52d0b7e5b6cf74d9edf5
Razarenov Aleksandar <razarenov.aleksandar@yandex.ru>
2022-09-01 23:57:55 +0300

    update websocket consumer, delete anonymous room

--- ws/consumers.py
+++ ws/consumers.py
@@ -6,26 +6,19 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class WSTest(AsyncWebsocketConsumer):
 
     async def connect(self):
-        self.scope['user'] = None if self.scope['user'].pk is None else self.scope['user']
         self.personal_room = str(self.scope['user'])
         self.general_room = 'general_room'
-        self.anonymous_room = 'anonymous_room'
 
-        if self.scope['user']:
-            await self.channel_layer.group_add(
-                self.personal_room,
-                self.channel_name
-            )
+        await self.channel_layer.group_add(
+            self.personal_room,
+            self.channel_name
+        )
 
-            await self.channel_layer.group_add(
-                self.general_room,
-                self.channel_name
-            )
-        else:
-            await self.channel_layer.group_add(
-                self.anonymous_room,
-                self.channel_name
-            )
+        await self.channel_layer.group_add(
+            self.general_room,
+            self.channel_name
+        )
+
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -39,22 +32,15 @@ class WSTest(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-        await self.channel_layer.group_discard(
-            self.anonymous_room,
-            self.channel_name
-        )
-
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        admin = text_data_json['admin']
 
-        if admin == 'no':
+        if text_data_json['admin'] == 'no':
             await self.channel_layer.group_send(
                 self.personal_room,
                 {
                     'type': 'message',
-                    'message': message
+                    'message': text_data_json['message']
                 }
             )
 
@@ -62,7 +48,7 @@ class WSTest(AsyncWebsocketConsumer):
             self.general_room,
             {
                 'type': 'message',
-                'message': message
+                'message': text_data_json['message']
             }
         )
 
