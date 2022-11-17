b9d395faf6cf68d2c9e20c556aa3d827f4dc586a
Razarenov Aleksandar <razarenov.aleksandar@yandex.ru>
2022-11-17 18:18:45 +0300

    update api like custom_shema

--- api/custom_schema/like/post.py
+++ api/custom_schema/like/post.py
@@ -2,15 +2,15 @@ from drf_yasg import openapi
 from rest_framework import status
 
 post_like_parameters = [
-    openapi.Parameter('photo_id', openapi.IN_FORM,
-                      description="The id of photo",
-                      type=openapi.TYPE_INTEGER,
+    openapi.Parameter('slug', openapi.IN_PATH,
+                      description="The slug of photo",
+                      type=openapi.TYPE_STRING,
                       required=True),
 ]
 
 post_like_response = {
     status.HTTP_201_CREATED: 'Successes',
-    status.HTTP_400_BAD_REQUEST: 'Incorrect photo_id values',
+    status.HTTP_404_NOT_FOUND: 'Incorrect  value of slug',
     status.HTTP_401_UNAUTHORIZED: 'Incorrect value of Api_token',
 }
 
