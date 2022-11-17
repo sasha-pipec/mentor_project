bbaf5c9183810df4081ea72c9f52fe4461df41fe
Razarenov Aleksandar <razarenov.aleksandar@yandex.ru>
2022-11-17 18:35:34 +0300

    update photobatle like service

--- photobatle/service/like/Create.py
+++ photobatle/service/like/Create.py
@@ -10,25 +10,28 @@ from api.status_code import *
 class CreateLikeService(ServiceWithResult):
     """Service class for create like"""
 
-    photo_id = forms.IntegerField()
+    slug = forms.SlugField(required=False)
     user_id = forms.IntegerField(required=False)
 
-    custom_validations = ["validate_user_id", "validate_photo_id", "validate_author_of_photo", "check_like"]
+    custom_validations = ["validate_user_id", "validate_slug", "validate_author_of_photo"]
 
     def process(self):
         self.run_custom_validations()
         if self.is_valid():
             self.result = self._create_like
+            self.response_status = status.HTTP_201_CREATED
         return self
 
-    def validate_photo_id(self):
+    def validate_slug(self):
         try:
-            Photo.objects.get(pk=self.cleaned_data['photo_id'])
+            Photo.objects.get(slug=self.cleaned_data['slug'])
         except Exception:
-            raise ValidationError404(f"Incorrect photo_id value")
+            if not self.cleaned_data['slug']:
+                raise ValidationError400(f'Missing one of all requirements parameters: slug')
+            raise ValidationError404(f"Incorrect value of slug")
 
     def validate_author_of_photo(self):
-        photo = Photo.objects.get(pk=self.cleaned_data['photo_id'])
+        photo = Photo.objects.get(slug=self.cleaned_data['slug'])
         if photo.user.id == self.cleaned_data['user_id']:
             raise ValidationError409(f"You can't like this photo, because it is your photo")
 
@@ -36,24 +39,20 @@ class CreateLikeService(ServiceWithResult):
         if not self.cleaned_data['user_id']:
             raise ValidationError401(f"incorrect api token")
 
-    def check_like(self):
-        if Like.objects.filter(photo_id=self.cleaned_data['photo_id'],
-                               user_id=self.cleaned_data['user_id']):
-            raise ValidationError409(f"Photo can have one like from one user")
-
     @property
     def _create_like(self):
-        Like.objects.create(photo_id=self.cleaned_data['photo_id'],
+        photo = Photo.objects.get(slug=self.cleaned_data['slug'])
+        Like.objects.create(photo_id=photo.id,
                             user_id=self.cleaned_data['user_id'])
         self.send_notification()
         return Photo.objects.annotate(comment_count=Count('comment_photo', distinct=True),
                                       like_count=Count('like_photo', distinct=True)).get(
-            pk=self.cleaned_data['photo_id'])
+            slug=self.cleaned_data['slug'])
 
     def send_notification(self):
         channel_layer = get_channel_layer()
         photo = Photo.objects.annotate(like_count=Count('like_photo')).get(
-            pk=self.cleaned_data['photo_id'])
+            slug=self.cleaned_data['slug'])
         username = str(User.objects.get(pk=self.cleaned_data['user_id']))
         async_to_sync(channel_layer.group_send)(
             str(photo.user), {
--- photobatle/service/like/Delete.py
+++ photobatle/service/like/Delete.py
@@ -10,47 +10,44 @@ from api.status_code import *
 class DeleteLikeService(ServiceWithResult):
     """Service class for delete like"""
 
-    photo_id = forms.IntegerField()
+    slug = forms.SlugField(required=False)
     user_id = forms.IntegerField(required=False)
 
-    custom_validations = ["validate_user_id", "validate_photo_id", "check_like"]
+    custom_validations = ["validate_user_id", "validate_slug"]
 
     def process(self):
         self.run_custom_validations()
         if self.is_valid():
             self.result = self._delete_like
+            self.response_status = status.HTTP_204_NO_CONTENT
         return self
 
     def validate_user_id(self):
         if not self.cleaned_data['user_id']:
             raise ValidationError401(f"incorrect api token")
 
-    def validate_photo_id(self):
+    def validate_slug(self):
         try:
-            return Photo.objects.get(pk=self.cleaned_data['photo_id'])
+            return Photo.objects.get(slug=self.cleaned_data['slug'])
         except Exception:
-            raise ValidationError404(f"Incorrect photo_id value")
-
-    def check_like(self):
-        try:
-            return Like.objects.get(photo_id=self.cleaned_data['photo_id'],
-                                    user_id=self.cleaned_data['user_id'])
-        except Exception:
-            raise ValidationError409(f"Photo dont have like from this user")
+            if not self.cleaned_data['slug']:
+                raise ValidationError400(f'Missing one of all requirements parameters: slug')
+            raise ValidationError404(f"Incorrect value of slug")
 
     @property
     def _delete_like(self):
-        like = Like.objects.get(photo_id=self.cleaned_data['photo_id'], user_id=self.cleaned_data['user_id'])
+        photo = Photo.objects.get(slug=self.cleaned_data['slug'])
+        like = Like.objects.get(photo_id=photo.id, user_id=self.cleaned_data['user_id'])
         like.delete()
         self.send_notification()
         return Photo.objects.annotate(comment_count=Count('comment_photo', distinct=True),
                                       like_count=Count('like_photo', distinct=True)).get(
-            pk=self.cleaned_data['photo_id'])
+            slug=self.cleaned_data['slug'])
 
     def send_notification(self):
         channel_layer = get_channel_layer()
         photo = Photo.objects.annotate(like_count=Count('like_photo')).get(
-            pk=self.cleaned_data['photo_id'])
+            slug=self.cleaned_data['slug'])
         username = str(User.objects.get(pk=self.cleaned_data['user_id']))
         async_to_sync(channel_layer.group_send)(
             str(photo.user), {
